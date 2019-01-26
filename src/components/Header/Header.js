319dc3c9a1d77ecbe24ae1e60b865405f242de59
Nawanshu Choudhary <nawanshu@gmail.com>
2019-01-26 16:23:05 +0530

    Output header with css styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import Icon from '@material-ui/core/Icon';
 
-import classes from 'components/Header/Header';
+import classes from 'components/Header/Header.css';
 import soundboksLogo from 'assets/images/logo.svg';
 
 const header = () => (
-    <div className={classes.Header}>
-        <img src={soundboksLogo} alt="SoundBoksLogo" />
-    </div>
+    <header className={classes.Header}>
+        <div className={classes.Logo}>
+            <a href="/">
+                <img src={soundboksLogo} alt="SoundBoksLogo" />
+            </a>
+        </div>
+        <nav className={classes.Navigation}>
+            <ul>
+                <li><Icon>shopping_cart</Icon></li>
+                <li><Icon>dehaze</Icon></li>
+            </ul>
+        </nav>
+    </header>
 );
 
 export default header;
