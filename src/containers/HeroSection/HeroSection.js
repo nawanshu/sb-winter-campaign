4f5bb522ecdee3f577d6cb1d373320f0d0d318a2
Nawanshu Choudhary <nawanshu@gmail.com>
2019-01-26 19:37:49 +0530

    Hero Section implement with loop video feature

--- src/containers/HeroSection/HeroSection.js
+++ src/containers/HeroSection/HeroSection.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
 
-import classes from './HeroSection.css';
+import classes from 'containers/HeroSection/HeroSection.css';
+import heroVideo from 'assets/images/Desktop_loop.mp4';
+import Aux from 'hoc/Auxiliary'
 
 class HeroSection extends Component {
 
     render () {
         return (
-            <div className={classes.HeroSection}>
-
-            </div>
+            <Aux>
+                <div className={classes.HeroSection}>
+                    <video poster="" id="vid">
+                        <source src={heroVideo} type="video/mp4" />
+                    </video>
+                </div>
+                <div className={classes.Content}>
+                    <h1>SEIZE<br />THE SEASON</h1>
+                    <Button variant="contained" color="primary" className={classes.button}>WATCH VIDEO</Button>
+                </div>
+            </Aux>
         );
     }
 }
