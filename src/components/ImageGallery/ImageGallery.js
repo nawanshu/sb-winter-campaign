659e788add862f8285db12f0fc4f3261b59d637d
Nawanshu Choudhary <nawanshu@gmail.com>
2019-01-28 00:16:59 +0530

    Add Image Gallery Component for gallery showcase above footer

---
+++ src/components/ImageGallery/ImageGallery.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import ImagesList from 'data/imagesList.json';
+import classes from 'components/ImageGallery/ImageGallery.css';
+import Aux from  'hoc/Auxiliary';
+
+const imageGallary = (props) => (
+    <Aux>
+        <h1 className={classes.ImageTitle}>#SEIZETHESEASON</h1>
+        <div className={classes.ImageGallery}>
+            {
+                ImagesList.map((imgSrc, index) => {
+                    return (
+                        <div className={classes.Container1} key={index}>
+                            <img src={imgSrc} alt="image1" />
+                        </div>
+                    );
+                })
+            }
+        </div>
+    </Aux>
+);
+
+export default imageGallary;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,11 +3,13 @@ import React from 'react';
 import Layout from 'components/Layout/Layout';
 import HeroSection from 'containers/HeroSection/HeroSection';
 import Products from 'containers/Products/Products';
+import ImageGallery from 'components/ImageGallery/ImageGallery';
 
 const home = () => (
     <Layout>
         <HeroSection />
         <Products />
+        <ImageGallery />
     </Layout>
 );
 
